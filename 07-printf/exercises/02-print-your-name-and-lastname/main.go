e39c5041591ba755b3da8fce6f9bc1f0f4acca81
Thang Nguyen <thangnguyen0722@gmail.com>
2025-10-08 11:24:32 +0700

    Finished

--- 07-printf/exercises/02-print-your-name-and-lastname/main.go
+++ 07-printf/exercises/02-print-your-name-and-lastname/main.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Print Your Name and LastName
 //
@@ -24,6 +26,8 @@ package main
 
 func main() {
 	// BONUS: Use a variable for the format specifier
+	var lastName = "Nguyen"
+	var firstName = "Thang"
 
-	// fmt.Printf("?", ?, ?)
+	fmt.Printf("My name is %s and my lastname is %s.\n", firstName, lastName)
 }
