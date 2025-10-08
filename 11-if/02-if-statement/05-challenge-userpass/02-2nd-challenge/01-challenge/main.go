c1291406ec92447f6cb6901fa55f8c596f41cf85
Thang Nguyen <thangnguyen0722@gmail.com>
2025-10-08 12:27:57 +0700

    Finished

--- 11-if/02-if-statement/05-challenge-userpass/02-2nd-challenge/01-challenge/main.go
+++ 11-if/02-if-statement/05-challenge-userpass/02-2nd-challenge/01-challenge/main.go
@@ -45,12 +45,12 @@ import (
 // ---------------------------------------------------------
 
 const (
-	usage    = "Usage: [username] [password]"
-	errUser  = "Access denied for %q.\n"
-	errPwd   = "Invalid password for %q.\n"
-	accessOK = "Access granted to %q.\n"
-	user     = "jack"
-	pass     = "1888"
+	usage       = "Usage: [username] [password]"
+	errUser     = "Access denied for %q.\n"
+	errPwd      = "Invalid password for %q.\n"
+	accessOK    = "Access granted to %q.\n"
+	user, user2 = "jack", "inanc"
+	pass, pass2 = "1888", "1879"
 )
 
 func main() {
@@ -63,11 +63,11 @@ func main() {
 
 	u, p := args[1], args[2]
 
-	if u != user {
+	if u != user && u != user2 {
 		fmt.Printf(errUser, u)
-	} else if p != pass {
-		fmt.Printf(errPwd, u)
-	} else {
+	} else if (u == user && p == pass) || (u == user2 && p == pass2) {
 		fmt.Printf(accessOK, u)
+	} else {
+		fmt.Printf(errPwd, u)
 	}
 }
